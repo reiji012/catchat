bdc427eb2db4b188c91f3028f72f3b70d9c968a1
rejii <reiji0120124@gmail.com>
2024-09-15 23:56:36 +0900

    firestoreの接続処理(仮)

--- catchat/lib/repositories/message_repository/message_repository.dart
+++ catchat/lib/repositories/message_repository/message_repository.dart
@@ -3,20 +3,40 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'model/message.dart';
 
 class MessageRepository {
-  // final FirebaseFirestore store = FirebaseFirestore.instance;
+  final FirebaseFirestore store = FirebaseFirestore.instance;
 
-  List<MessageModel> getMessages() {
-    // return store.collection('messages').snapshots().map((snapshot) {
-    //   return snapshot.docs
-    //       .map((doc) => MessageModel.fromJson(doc.data()))
-    //       .toList();
-    // });
+  Future<List<MessageModel>?> getMessages() async {
+    // final snapshot = await store.collection('messages').doc('1').get();
+    // return snapshot
+    //     .data()!['messages']
+    //     .map((doc) => MessageModel.fromJson(doc.data()))
+    //     .toList();
 
-    final messages = [
-      MessageModel(content: "こんにちは！", isMe: true),
-      MessageModel(content: "こんにちは、何かようでち？", isMe: false),
-    ];
+    // final messages = [
+    //   MessageModel(content: "こんにちは！", isMe: true),
+    //   MessageModel(content: "こんにちは、何かようでち？", isMe: false),
+    // ];
 
-    return messages;
+    // return messages;
+
+    List<MessageModel> messages = [];
+
+    try {
+      await store
+          .collection("chats")
+          .doc('1')
+          .collection('messages')
+          .get()
+          .then((value) {
+        print("MessageRepository: FireStoreを取得できました");
+        print("value: ${value}");
+        messages =
+            value.docs.map((e) => MessageModel.fromJson(e.data())).toList();
+      });
+      print("MessageModel: ${messages}");
+      return messages;
+    } catch (e) {
+      return null;
+    }
   }
 }
